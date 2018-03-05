f3e6f9917ea013d99735ca0d32a0dbf264ee3b48
littleflute <13699175164@sina.cn>
2018-03-05 13:58:48 -0800

    Add files via upload

---
+++ mp3/MereChristianity/cd5/plx.js
@@ -0,0 +1,4 @@
+var _v = "v0.0.3";
+var plxUI = blo0.blMDiv(bl$("id_div_wrap_4_plx" ),"id_mdiv_4_plx","plx_" + _v,50,50,500,50,"green");	
+plxUI.v1 = blo0.blDiv(plxUI,plxUI.id + "v1", "v1",blColor[0]);
+
