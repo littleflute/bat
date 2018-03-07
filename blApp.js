bc82d5c01ebecebbea0e9c4bbbc30ea40253aad0
littleflute <13699175164@sina.cn>
2018-03-07 13:05:21 -0800

    Update blApp.js

--- blApp.js
+++ blApp.js
@@ -1,19 +1,7 @@
-function _myJobClass()
-{
-	this.v = "v0.0.5";
-	this.blrAboutMe = function(b,d){
-		if(!d.v){
-			var s = 'blrAboutMe:<br> '; 
-     			s += '<a href="blApp.js" target="_blank">blApp.js</a> - ';
-     			s += '<a href="https://github.com/littleflute/bat/edit/master/blApp.js" target="_blank">blApp.js*</a>';
-			d.v	= blo0.blDiv(d,d.id+"v",s,blColor0]);
-		}
-		_on_off_div(b,d);b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
-	}
-}
+ 
 function CXdMainFrame()
 {
-	var mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "mfUI: v0.1. 152 " , 0,0,150,50, blColor[2]);  
+	var mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "mfUI: v0.1. 153 " , 0,0,150,50, blColor[2]);  
 	mfUI.style.position = "fixed";
 	mfUI.style.top = "50px";
 	mfUI.style.left = "1px"; 
@@ -30,24 +18,7 @@ function CXdMainFrame()
 		_on_off_div(this,mfUI.d);
 	}
  	mfUI.b1.click();
- 	mfUI.b1.click();
-
-	mfUI.d.d2	= blo0.blDiv(mfUI.d,mfUI.d.id+"d2","mfUI.d.d2",blColor[1]);  		 
-
-	_load_plx_btn(blo0,mfUI.d.d2,"_xdPlxSet","http://www.beautifullover.org/zb_users/theme/HTML5CSS3/script/xdPlxSet.js","lightblue"); 
-	CXdBtn_Handle_Main ( 'id_div_toolbar_4_blApp' ); 
+ 	mfUI.b1.click(); 
 }
-
-function CXdBtn_Handle_Main (idDivBoss)
-{ 
-	var o	= bl$(idDivBoss);  
-	o.hm	= blo0.blDiv(o,o.id+"hm","",blColor[1]); 
-
-	o.b2 = blo0.blBtn(o.hm,o.hm.id+"b2","hm",blGrey[0]);
-	o.b2.onclick = function(){
-		var d = bl$("id_div_CXdMainFrame_ui");	_on_off_div(this,d.handle); _on_off_div(this,d.main);
-	} 
-	o.b2.click();	o.b2.click();
-}
-
+ 
 var f = new CXdMainFrame;    
