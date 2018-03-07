7731cd17194cae16da86d7c2cd1a36ae80f6bc47
littleflute <13699175164@sina.cn>
2018-03-07 13:14:58 -0800

    Update blApp.js

--- blApp.js
+++ blApp.js
@@ -5,7 +5,7 @@ function _myJobClass()
 }
 function CXdMainFrame()
 {
-	var mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "mfUI: v0.1. 212 " , 0,0,150,50, blColor[2]);  
+	var mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "mfUI: v0.1. 213 " , 0,0,150,50, blColor[2]);  
 	mfUI.style.position = "fixed"; 
 	mfUI.b1 = blo0.blBtn(mfUI,mfUI.id+"b1","+",this.v,blGrey[0]);
 	mfUI.v 	= blo0.blDiv(mfUI,mfUI.id + "v" , "mfUI.v" ,0,150,190,20,blColor[1]);   
