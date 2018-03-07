e4055cd966c2f6882302d7c02f633c9b3c719de4
littleflute <13699175164@sina.cn>
2018-03-07 00:21:59 -0800

    Update plx.js

--- mp3/MereChristianity/cd1/plx.js
+++ mp3/MereChristianity/cd1/plx.js
@@ -1,13 +1,13 @@
-var _v = "v0.0.11";
+var _v = "v0.0.12";
 var s = "plx_" + _v;
 s += "<a target = '_blank' href='https://littleflute.github.io/bat/mp3/MereChristianity/cd1/plx.js'>plx.js</a> - ";
 s += "<a target = '_blank' href='https://github.com/littleflute/bat/edit/master/mp3/MereChristianity/cd1/plx.js'>plx.js*</a>";
 
 var plxUI = blo0.blMDiv(bl$("id_div_wrap_4_plx" ),"id_mdiv_4_plx",s,50,50,500,50,"green");	
 plxUI.v1 = blo0.blDiv(plxUI,plxUI.id + "v1", "v1",blColor[0]);
-blo0.blShowObj2Div(plxUI.v1,_myJob);
-
+//---------------------------------------------------------------------------
 function _myJobClass (){
   this.v = "v0.0.1";
 }
 var _myJob = new _myJobClass;
+blo0.blShowObj2Div(plxUI.v1,_myJob);
