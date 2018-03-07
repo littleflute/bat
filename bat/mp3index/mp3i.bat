d643cc2f4a15032d0b0962f4b723c9ff079a86b0
littleflute <13699175164@sina.cn>
2018-03-07 03:12:23 -0800

    Update mp3i.bat

--- bat/mp3index/mp3i.bat
+++ bat/mp3index/mp3i.bat
@@ -3,7 +3,7 @@ echo ^<!DOCTYPE html^>  >>index.html
 echo ^<html^>		>>index.html
 echo ^<head^>		>>index.html
 echo ^<title^>		>>index.html
-echo v0.1.12		>>index.html 
+echo v0.1.13		>>index.html 
 echo ^</title^>		>>index.html
 
 echo ^<style^>				>>index.html
@@ -141,7 +141,7 @@ for /F "delims=" %%a in ('dir /b *.mp3') do (
 )
  
 
-echo ^<br^>^<audio id="mp3Player" controls^> 	>>index.html
+echo ^<br^>^<audio id="mp3Player0" controls^> 	>>index.html
 echo 	^<source src="01 Owner of a Lonely Heart.mp3" type="audio/mpeg"^>	>>index.html
 echo	Your browser does not supoort hte audio element!	>>index.html
 echo ^</audio^>		>>index.html
