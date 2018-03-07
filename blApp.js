3b8ab5e710db4e11a205d79e84402c3eaa3e2d72
littleflute <13699175164@sina.cn>
2018-03-07 12:39:49 -0800

    Update blApp.js

--- blApp.js
+++ blApp.js
@@ -4,7 +4,7 @@ function _myJobClass()
 }
 function CXdMainFrame()
 {
-	var mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "CXdMainFrame - mfUI: v0.1. 135 " , 0,0,50,50, blColor[2]);  
+	var mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "mfUI: v0.1. 142 " , 0,0,50,50, blColor[2]);  
 	mfUI.style.position = "fixed";
 	mfUI.style.top = "50px";
 	mfUI.style.left = "1px"; 
@@ -13,6 +13,8 @@ function CXdMainFrame()
 	mfUI.b1 = blo0.blBtn(mfUI,mfUI.id+"b1","+",this.v,blGrey[0]);
 	var s = "<div id = 'id_div_toolbar_4_blApp' style='width:100%;height:50px;'>v0.1. 51 </div>"; 
 	mfUI.d 	= blo0.blMDiv(mfUI, "id_div_CXdMainFrame_ui" ,s,0,150,190,20,blColor[1]);   
+	mfUI.d.jobView = blo0.blDiv(mfUI.d,mfUI.d.id + "jobView","jobView",blColor[4]);
+	blo0.blShowObj2Div(mfUI.d.jobView,new _myJobClass);
 	mfUI.d.followDiv = mfUI;
 
 	mfUI.b1.onclick = function(){
