55a109796f52ad2d593683d8cb60565a7cf8e769
littleflute <13699175164@sina.cn>
2018-03-07 22:54:29 -0800

    Update blAppPlx.js

--- blAppPlx.js
+++ blAppPlx.js
@@ -1,8 +1,27 @@
-  
+var _g_ver_blAppPlx = "v0.1.11";
+
 function _plxJobClass()
-{
-	this.v = "v0.0.2";
-	
+{ 
+	this.blrIssue3C = function(b,d){
+		if(!d.v){
+			d.v = blo0.blDiv(d,d.id+"v","v",blGrey[5]);
+			d.v.innerHTML = "blrIssue3C <br>"; 
+			function _loadIssue3Comments(o) {
+				for(i in o){
+					var a = o[i].body;
+					var b = a.split("~~~html");
+					var c = b[1].split("~~~"); 
+					d.v.innerHTML += c[0]; 
+					d.v.innerHTML += "<br>";
+				}
+
+			}
+			w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/6/comments", _loadIssue3Comments);		
+		}
+		_on_off_div(b,d);
+		
+		b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
+	}
 	this.blrAboutPlx= function(b,d){		
 		var s = "blrAboutPlx<br>"; 
 		s += "<a target='_blank' href='https://github.com/littleflute/bat/edit/master/blAppPlx.js'"
@@ -26,7 +45,7 @@ function CBlAppPlxClass()
 	var _plxUI = null;
 	var app = bl$("id_div_CXdMainFrame");
 	if(app!=null){
-		_plxUI = blo0.blDiv(app, app.id + "_plx" , "blAppPlx: v0.0. 13 " , blColor[3]);  
+		_plxUI = blo0.blDiv(app, app.id + "_plx" , "blAppPlx: " + _g_ver_blAppPlx, blColor[3]);  
 		_plxUI.v = blo0.blDiv(_plxUI, _plxUI.id + "_v" , "v " , blColor[4]);  
 		blo0.blShowObj2Div(_plxUI.v,new _plxJobClass);
 	} 
