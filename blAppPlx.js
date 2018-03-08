60d8be34480e70d77b498808305bb7f99917b217
littleflute <13699175164@sina.cn>
2018-03-07 22:46:22 -0800

    Update blAppPlx.js

--- blAppPlx.js
+++ blAppPlx.js
@@ -1,9 +1,16 @@
- 
+  
+function _plxJobClass()
+{
+	this.v = "v0.0.1";
+}
 function CBlAppPlxClass()
 { 
+	var _plxUI = null;
 	var app = bl$("id_div_CXdMainFrame");
 	if(app!=null){
-		var mfUI = blo0.blMDiv(app, app.id + "_plx" , "blAppPlx: v0.0. 11 " , 0,0,150,50, blColor[3]);  
+		_plxUI = blo0.blDiv(app, app.id + "_plx" , "blAppPlx: v0.0. 12 " , blColor[3]);  
+		_plxUI.v = blo0.blDiv(_plxUI, _plxUI.id + "_v" , "v " , blColor[4]);  
+		blo0.blShowObj2Div(_plxUI.v,new _plxJobClass);
 	} 
 	else{
 		alert("no 'id_div_CXdMainFrame' !");
