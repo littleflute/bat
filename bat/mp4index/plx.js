ac352534cfa19f411b57b99cb6ed1947785155fd
littleflute <13699175164@sina.cn>
2018-04-07 11:44:04 -0700

    Add files via upload

--- bat/mp4index/plx.js
+++ bat/mp4index/plx.js
@@ -1,2 +1,9 @@
-//plx.js
-alert("plx.js");
+//plx.js 
+var _div = function (id, html){					
+	var	r = document.createElement("div");		
+	r.id = id;				
+	r.innerHTML = html; 				
+	document.body.appendChild(r);	 	
+}				
+	
+_div ("id_div_1"," plx.js : v0.0. 5 ");	
