d684133376cd2a19e19d77c4130b46c075146727
littleflute <13699175164@sina.cn>
2018-04-18 12:16:25 -0700

    Update mp4i.bat

--- bat/mp4index/mp4i.bat
+++ bat/mp4index/mp4i.bat
@@ -6,7 +6,7 @@ echo ^<html^> >>index.html
 echo ^<body^> >>index.html
 echo %1 >>index.html
 
-echo ^<div id="id_div_mp4i_bat_info" ^> ^<a target="_blank" href="https://github.com/littleflute/bat/edit/master/bat/mp4index/mp4i.bat"^>v1.2. 23 * ^</a^> ^</div^>			 >>index.html
+echo ^<div id="id_div_mp4i_bat_info" ^> ^<a target="_blank" href="https://github.com/littleflute/bat/edit/master/bat/mp4index/mp4i.bat"^>v1.2. 34 * ^</a^> ^</div^>			 >>index.html
 echo ^<div id="DivCurTime"^>^</div^> >>index.html
 echo ^<br^> >>index.html
 
@@ -38,9 +38,21 @@ for /F "delims="  %%a in ('dir /b *.mp4') do (
 	call set /a x = !x! + 1
 	echo ^<button onclick="play('%%a')"^>!x!:%%a^</button^> >>index.html
 )
-
-
 @echo off  
+
+echo ^<br^> 	>>index.html
+echo ^<div id="id_div_4_jpg" style="border:1px solid blue;" ^>				>>index.html
+@echo on
+setlocal ENABLEDELAYEDEXPANSION
+call set /a x = 0
+for /F "delims="  %%a in ('dir /b *.jpg') do (
+	rem echo %%a >>index.html 
+	call set /a x = !x! + 1
+	echo %%a, >>index.html
+)
+@echo off  
+echo ^</div^> 											>>index.html
+
 echo ^<br^> >>index.html
 echo ^<video id="myVideo" width="720" height="480" controls^> >>index.html
 echo  ^<source src="0.mp4" type="video/mp4"^>  >>index.html
