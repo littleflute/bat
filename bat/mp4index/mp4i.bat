56383594bacd03e960ea26415761abf78e0563c6
littleflute <13699175164@sina.cn>
2018-06-12 11:55:02 -0700

    Update mp4i.bat

--- bat/mp4index/mp4i.bat
+++ bat/mp4index/mp4i.bat
@@ -4,21 +4,13 @@ del index.html
 echo ^<!DOCTYPE html^>	>>index.html  
 echo ^<html^> >>index.html 
 echo ^<body^> >>index.html
-echo %1 >>index.html
+ 
 
 echo ^<script src="https://www.w3schools.com/lib/w3.js"^>^</script^>		>>index.html
 
-echo ^<div id="id_div_mp4i_bat_info" ^> ^<a target="_blank" href="https://github.com/littleflute/bat/edit/master/bat/mp4index/mp4i.bat"^>v1.2. 41 * ^</a^> ^</div^>			 >>index.html
+echo ^<div id="id_div_mp4i_bat_info" ^> ^<a target="_blank" href="https://github.com/littleflute/bat/edit/master/bat/mp4index/mp4i.bat"^>v1.2. 54 * ^</a^> ^</div^>			 >>index.html
 echo ^<div id="DivCurTime"^>^</div^> >>index.html
-echo ^<br^> >>index.html
-
-echo ^<div id="id_div_toolbar_1" style="border:1px solid red;" ^>							>>index.html
-echo ^<button onclick="getCurTime()" type="button"^>Get current time position^</button^> >>index.html 
-
-echo ^<button onclick="setCurTime(0)" type="button"^>00^</button^>^<br^> >>index.html  
- 
-echo ^<button onclick="setCurTime(5)" type="button"^>Set time position to 5 seconds^</button^>^<br^>   >>index.html
-echo ^</div^> 														>>index.html
+echo ^<br^> >>index.html 
 
 @echo on
 setlocal ENABLEDELAYEDEXPANSION
@@ -92,15 +84,16 @@ echo     vid.src=i; >>index.html
 echo     vid.load();>>index.html
 echo     vid.play(); >>index.html
 echo }>>index.html
- 
-
+  
 echo function getCurTime() {  >>index.html
 echo    document.getElementById("DivCurTime").innerHTML= vid.currentTime; >>index.html
 echo }   >>index.html
+
 echo function setCurTime(t) {>>index.html 
 echo    vid.currentTime=t;  >>index.html
 echo    getCurTime();>>index.html
 echo }  >>index.html
+
 echo function moveMS(ms) {>>index.html 
 echo   vid.currentTime+=ms;>>index.html
 echo   getCurTime();  >>index.html
