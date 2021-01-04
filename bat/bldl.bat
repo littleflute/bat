0e674ff45a2133cbbbf736b3f4ca50029842462a
littleflute <littleflute@163.com>
2021-01-04 14:10:20 -0800

    bldl.bat

--- bat/bldl.bat
+++ bat/bldl.bat
@@ -1,4 +1,4 @@
-rd blbat/s/q
-md blbat
 
-curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    -o 		blbat\bldl.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    		-o 		bldl.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/srt2v.bat	    	-o 		srt2v.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/gif2v.bat	    	-o 		gif2v.bat
