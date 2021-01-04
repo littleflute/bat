7584a37964b639daabe4da1fa4b7c14b0d325130
littleflute <littleflute@163.com>
2021-01-04 13:18:27 -0800

    bldl.bat*

--- bat/bldl.bat
+++ bat/bldl.bat
@@ -1,4 +1,4 @@
 rd blbat/s/q
 md blbat
 
-curl https://raw.githubusercontent.com/littleflute/bat/master/bldl.bat		    -o 		blbat\bldl.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    -o 		blbat\bldl.bat
