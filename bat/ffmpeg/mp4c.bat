08d222837efc3a165b2618bb526f621ea56ccdcc
littleflute <littleflute@163.comd>
2021-01-07 15:38:53 -0800

    mp4c*

--- bat/ffmpeg/mp4c.bat
+++ bat/ffmpeg/mp4c.bat
@@ -1,1 +1,14 @@
-dir 
+del mp4list.txt
+rem ------------ echo mp4c.bat v0.0. 31 ; >> mp4list.txt
+
+rem step1: replace space with ''
+@echo on
+setlocal ENABLEDELAYEDEXPANSION
+for %%a in ("* *.mp4") do (
+set file=%%a
+ren "!file!" "!file: =!"
+
+)
+
+rem ----------- step2: write list to mp4list.txt
+
