c7931edc225b828a47aa6485f73b6f9ebe1c61a6
littleflute <littleflute@163.comd>
2021-01-07 15:54:23 -0800

    mp4c v0.0.33

--- bat/ffmpeg/mp4c.bat
+++ bat/ffmpeg/mp4c.bat
@@ -1,5 +1,5 @@
 del mp4list.txt
-rem ------------ echo mp4c.bat v0.0. 32 ; >> mp4list.txt
+rem ------------ echo mp4c.bat v0.0. 33 ;           >>      mp4list.txt
 
 rem step1: replace space with ''
 @echo on
@@ -16,6 +16,10 @@ call set /a x = 0
 for /F "delims=" %%a in ('dir /b *.mp4') do (
 call set /a x = !x! + 1
 rem echo !x!
-echo file '%%a' >> mp4list.txt
+echo file '%%a'                                     >>      mp4list.txt
 )
 @echo off
+
+
+rem -------- step3 ----- run ffmpeg command
+ffmpeg -f concat -i mp4list.txt -c copy all.mp4
