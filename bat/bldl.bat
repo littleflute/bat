faff3395e0a5cafe64885dd19107f20a4ac4d4fa
littleflute <littleflute@163.comd>
2021-01-07 15:32:42 -0800

    bldl.bat

--- bat/bldl.bat
+++ bat/bldl.bat
@@ -3,7 +3,7 @@ curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    	
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/srt2v.bat	    	                        -o 		srt2v.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/gif2v.bat	    	                        -o 		gif2v.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/logo2v.bat	                              	-o 		logo2v.bat
-curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/mp4c.bat	                              	-o 		logo2v.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/mp4c.bat	                              	-o 		mp4c.bat
 
 curl https://raw.githubusercontent.com/littleflute/blog/master/i/i1250/1138.gif		                            	-o 		1138.gif
 curl https://user-images.githubusercontent.com/17950965/103499626-9f52cb00-4dfd-11eb-9088-56e3fef3dd03.png	    	-o 		dt01.png
