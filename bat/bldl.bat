8aea7205c068c7b0cc06dfb6bbdac82b52aef5c2
littleflute <littleflute@163.com>
2021-01-08 13:42:33 -0800

    bldl*

--- bat/bldl.bat
+++ bat/bldl.bat
@@ -1,5 +1,5 @@
 
-curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    		                            -o 		bldl.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/mv.bat	    		                        -o 		mv.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/srt2v.bat	    	                        -o 		srt2v.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/gif2v.bat	    	                        -o 		gif2v.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/logo2v.bat	                              	-o 		logo2v.bat
