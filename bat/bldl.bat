02d07f209364cb2ab0c49d0424e5e1e99fd13284
unknown <yongling.huang@group6.io>
2021-01-19 00:13:33 -0800

    ap2v.bat

--- bat/bldl.bat
+++ bat/bldl.bat
@@ -1,5 +1,6 @@
 
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/mv.bat	    		                        -o 	mv.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/ap2v.bat	    	                        -o 	ap2v.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/srt2v.bat	    	                        -o 	srt2v.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/gif2v.bat	    	                        -o 	gif2v.bat
 curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/logo2v.bat	                              	-o 	logo2v.bat
