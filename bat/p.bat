d869a2af4ef31866dfea8114e4ebb40296a43eaf
littleflute <littleflute@163.com>
2021-09-03 09:56:41 +0800

    p.bat*

--- bat/p.bat
+++ bat/p.bat
@@ -1,4 +1,4 @@
 path=%path%;"C:\Users\wangyi7\xd\gh\bat\bat";
-path=%path%;"C:\Program Files\Google\Chrome\Application";
-
+path=%path%;"C:\Program Files\Google\Chrome\Application"; 
+path=%path%;%MAVEN_HOME%\bin
 
