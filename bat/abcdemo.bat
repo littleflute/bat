83ab65514f2196248b906c29b4cbbed6006d98b6
littleflute <littleflute@163.com>
2022-08-17 23:53:02 +0800

    abcdemo.bat+

---
+++ bat/abcdemo.bat
@@ -0,0 +1,7 @@
+rd 		c:\FFOutput\abcDemo /s/q
+md 		c:\FFoutput\abcDemo
+copy 		C:\Users\13699\xd\gh\littleflute\Games\node1\public\abc\xd4xa\d1.html		c:\FFoutput\abcDemo
+copy 		C:\Users\13699\xd\gh\littleflute\Games\node1\public\abc\xd4xa\*.js		c:\FFoutput\abcDemo
+copy 		C:\Users\13699\xd\gh\littleflute\Games\node1\public\abc\xd4xa\*.css		c:\FFoutput\abcDemo
+md 		c:\FFoutput\abcDemo\Scc1t2
+copy 		C:\Users\13699\xd\gh\littleflute\Games\node1\public\abc\xd4xa\Scc1t2\*.*	c:\FFoutput\abcDemo\Scc1t2
